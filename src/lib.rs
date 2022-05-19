8e17f8f1d1486e68aee07178e5831cdc7d7a08b0
Junho Yeo <i@junho.io>
2022-05-19 12:44:51 +0900

    Add previous implementation at Terra

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,187 @@
-pub mod contract;
-mod error;
-pub mod helpers;
-pub mod integration_tests;
 pub mod msg;
 pub mod state;
 
-pub use crate::error::ContractError;
+use crate::msg::{Cw721MetadataContract, ExecuteMsg, Extension, Metadata, MintMsgInput};
+use crate::state::{State, STATE};
+
+use cw721_base::state::TokenInfo;
+pub use cw721_base::{ContractError, InstantiateMsg, MintMsg, MinterResponse, QueryMsg};
+
+#[cfg(not(feature = "library"))]
+pub mod entry {
+    use super::*;
+
+    use cosmwasm_std::entry_point;
+    use cosmwasm_std::{Binary, Deps, DepsMut, Env, MessageInfo, Response, StdResult};
+
+    // This is a simple type to let us handle empty extensions
+
+    // This makes a conscious choice on the various generics used by the contract
+    #[entry_point]
+    pub fn instantiate(
+        deps: DepsMut,
+        env: Env,
+        info: MessageInfo,
+        msg: InstantiateMsg,
+    ) -> StdResult<Response> {
+        let state = State { token_count: 1 };
+        STATE.save(deps.storage, &state)?;
+        Cw721MetadataContract::default().instantiate(deps, env, info, msg)
+    }
+
+    #[entry_point]
+    pub fn execute(
+        deps: DepsMut,
+        env: Env,
+        info: MessageInfo,
+        msg: ExecuteMsg,
+    ) -> Result<Response, ContractError> {
+        match msg {
+            ExecuteMsg::Burn { token_id } => execute_burn(deps, env, info, token_id),
+            ExecuteMsg::Mint(mint_msg) => execute_mint(deps, env, info, mint_msg),
+            _ => Cw721MetadataContract::default().execute(
+                deps,
+                env,
+                info,
+                cw721_base::ExecuteMsg::<std::option::Option<Metadata>>::from(msg),
+            ),
+        }
+    }
+
+    #[entry_point]
+    pub fn query(deps: Deps, env: Env, msg: QueryMsg) -> StdResult<Binary> {
+        Cw721MetadataContract::default().query(deps, env, msg)
+    }
+
+    pub fn execute_mint(
+        deps: DepsMut,
+        env: Env,
+        info: MessageInfo,
+        mint_msg_input: MintMsgInput<Extension>,
+    ) -> Result<Response, ContractError> {
+        let cw721_contract = Cw721MetadataContract::default();
+
+        let _state = STATE.load(deps.storage)?;
+        let token_id = _state.token_count.to_string();
+        STATE.update(deps.storage, |mut __state| -> Result<_, ContractError> {
+            __state.token_count += 1;
+            Ok(__state)
+        })?;
+
+        let response = cw721_contract.mint(
+            deps,
+            env,
+            info,
+            MintMsg {
+                token_id,
+                token_uri: mint_msg_input.token_uri,
+                owner: mint_msg_input.owner,
+                extension: mint_msg_input.extension,
+            },
+        )?;
+        Ok(response)
+    }
+
+    pub fn execute_burn(
+        deps: DepsMut,
+        env: Env,
+        info: MessageInfo,
+        token_id: String,
+    ) -> Result<Response, ContractError> {
+        let cw721_contract = Cw721MetadataContract::default();
+
+        let token = cw721_contract.tokens.load(deps.storage, &token_id)?;
+        _check_can_send(&cw721_contract, deps.as_ref(), &env, &info, &token)?;
+
+        cw721_contract.tokens.remove(deps.storage, &token_id)?;
+        cw721_contract
+            .token_count
+            .update(deps.storage, |count| -> Result<u64, ContractError> {
+                Ok(count - 1)
+            })?;
+
+        Ok(Response::new()
+            .add_attribute("action", "burn")
+            .add_attribute("token_id", token_id))
+    }
+
+    fn _check_can_send<T>(
+        cw721_contract: &Cw721MetadataContract,
+        deps: Deps,
+        env: &Env,
+        info: &MessageInfo,
+        token: &TokenInfo<T>,
+    ) -> Result<(), ContractError> {
+        if token.owner == info.sender {
+            return Ok(());
+        }
+
+        if token
+            .approvals
+            .iter()
+            .any(|apr| apr.spender == info.sender && !apr.is_expired(&env.block))
+        {
+            return Ok(());
+        }
+
+        let op = cw721_contract
+            .operators
+            .may_load(deps.storage, (&token.owner, &info.sender))?;
+        match op {
+            Some(ex) => {
+                if ex.is_expired(&env.block) {
+                    Err(ContractError::Unauthorized {})
+                } else {
+                    Ok(())
+                }
+            }
+            None => Err(ContractError::Unauthorized {}),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use cosmwasm_std::testing::{mock_dependencies, mock_env, mock_info};
+    use cw721::Cw721Query;
+
+    const CREATOR: &str = "creator";
+
+    #[test]
+    fn use_metadata_extension() {
+        let mut deps = mock_dependencies(&[]);
+        let contract = Cw721MetadataContract::default();
+
+        let info = mock_info(CREATOR, &[]);
+        let init_msg = InstantiateMsg {
+            name: "SpaceShips".to_string(),
+            symbol: "SPACE".to_string(),
+            minter: CREATOR.to_string(),
+        };
+        contract
+            .instantiate(deps.as_mut(), mock_env(), info.clone(), init_msg)
+            .unwrap();
+
+        let token_id = "Enterprise";
+        let mint_msg = MintMsg {
+            token_id: token_id.to_string(),
+            owner: "john".to_string(),
+            token_uri: Some("https://starships.example.com/Starship/Enterprise.json".into()),
+            extension: Some(Metadata {
+                description: Some("Spaceship with Warp Drive".into()),
+                name: Some("Starship USS Enterprise".to_string()),
+                ..Metadata::default()
+            }),
+        };
+        let exec_msg = cw721_base::ExecuteMsg::Mint(mint_msg.clone());
+        contract
+            .execute(deps.as_mut(), mock_env(), info, exec_msg)
+            .unwrap();
+
+        let res = contract.nft_info(deps.as_ref(), token_id.into()).unwrap();
+        assert_eq!(res.token_uri, mint_msg.token_uri);
+        assert_eq!(res.extension, mint_msg.extension);
+    }
+}
--- src/msg.rs
+++ src/msg.rs
@@ -1,27 +1,132 @@
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-pub struct InstantiateMsg {
-    pub count: i32,
+use cosmwasm_std::{Binary, Empty};
+use cw721::Expiration;
+pub use cw721_base::{ContractError, InstantiateMsg, MintMsg, MinterResponse, QueryMsg};
+
+#[derive(Serialize, Deserialize, Clone, PartialEq, JsonSchema, Debug, Default)]
+pub struct Trait {
+    pub display_type: Option<String>,
+    pub trait_type: String,
+    pub value: String,
 }
 
+// see: https://docs.opensea.io/docs/metadata-standards
+#[derive(Serialize, Deserialize, Clone, PartialEq, JsonSchema, Debug, Default)]
+pub struct Metadata {
+    pub image: Option<String>,
+    pub image_data: Option<String>,
+    pub external_url: Option<String>,
+    pub description: Option<String>,
+    pub name: Option<String>,
+    pub attributes: Option<Vec<Trait>>,
+    pub background_color: Option<String>,
+    pub animation_url: Option<String>,
+    pub youtube_url: Option<String>,
+}
+
+pub type Extension = Option<Metadata>;
+pub type Cw721MetadataContract<'a> = cw721_base::Cw721Contract<'a, Extension, Empty>;
+
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct MintMsgInput<T> {
+    /// Unique ID of the NFT
+    // pub token_id: String,
+
+    /// The owner of the newly minter NFT
+    pub owner: String,
+    /// Universal resource identifier for this NFT
+    /// Should point to a JSON file that conforms to the ERC721
+    /// Metadata JSON Schema
+    pub token_uri: Option<String>,
+    /// Any custom extension used by this contract
+    pub extension: T,
+}
+
+#[derive(Serialize, Deserialize, Clone, PartialEq, JsonSchema, Debug)]
 #[serde(rename_all = "snake_case")]
 pub enum ExecuteMsg {
-    Increment {},
-    Reset { count: i32 },
+    Burn {
+        token_id: String,
+    },
+
+    /// Mint a new NFT, can only be called by the contract minter
+    Mint(MintMsgInput<Extension>),
+
+    // Standard CW721 ExecuteMsg
+    /// Transfer is a base message to move a token to another account without triggering actions
+    TransferNft {
+        recipient: String,
+        token_id: String,
+    },
+    /// Send is a base message to transfer a token to a contract and trigger an action
+    /// on the receiving contract.
+    SendNft {
+        contract: String,
+        token_id: String,
+        msg: Binary,
+    },
+    /// Allows operator to transfer / send the token from the owner's account.
+    /// If expiration is set, then this allowance has a time/height limit
+    Approve {
+        spender: String,
+        token_id: String,
+        expires: Option<Expiration>,
+    },
+    /// Remove previously granted Approval
+    Revoke {
+        spender: String,
+        token_id: String,
+    },
+    /// Allows operator to transfer / send any token from the owner's account.
+    /// If expiration is set, then this allowance has a time/height limit
+    ApproveAll {
+        operator: String,
+        expires: Option<Expiration>,
+    },
+    /// Remove previously granted ApproveAll permission
+    RevokeAll {
+        operator: String,
+    },
 }
 
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-#[serde(rename_all = "snake_case")]
-pub enum QueryMsg {
-    // GetCount returns the current count as a json-encoded number
-    GetCount {},
-}
-
-// We define a custom struct for each query response
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-pub struct CountResponse {
-    pub count: i32,
+impl From<ExecuteMsg> for cw721_base::ExecuteMsg<Extension> {
+    fn from(msg: ExecuteMsg) -> cw721_base::ExecuteMsg<Extension> {
+        match msg {
+            ExecuteMsg::TransferNft {
+                recipient,
+                token_id,
+            } => cw721_base::ExecuteMsg::TransferNft {
+                recipient,
+                token_id,
+            },
+            ExecuteMsg::SendNft {
+                contract,
+                token_id,
+                msg,
+            } => cw721_base::ExecuteMsg::SendNft {
+                contract,
+                token_id,
+                msg,
+            },
+            ExecuteMsg::Approve {
+                spender,
+                token_id,
+                expires,
+            } => cw721_base::ExecuteMsg::Approve {
+                spender,
+                token_id,
+                expires,
+            },
+            ExecuteMsg::Revoke { spender, token_id } => {
+                cw721_base::ExecuteMsg::Revoke { spender, token_id }
+            }
+            ExecuteMsg::ApproveAll { operator, expires } => {
+                cw721_base::ExecuteMsg::ApproveAll { operator, expires }
+            }
+            ExecuteMsg::RevokeAll { operator } => cw721_base::ExecuteMsg::RevokeAll { operator },
+            _ => panic!("cannot covert {:?} to cw721_base::ExecuteMsg", msg),
+        }
+    }
 }
